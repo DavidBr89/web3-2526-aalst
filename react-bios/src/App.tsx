09b0b2b00576b978a9737c565fcc57f76eca22b1
David Breckx <david.breckx@telenet.be>
2025-10-20 10:11:03 +0200

    Pauze les 5

--- react-bios/src/App.tsx
+++ react-bios/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import movies from "./data/films.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [films, setFilms] = useState([]);
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/popular",
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGYyZjgzNWQ4MDg5Y2Y5YTczMTk3YTJhMWRkYWJiMyIsIm5iZiI6MTYwNzA4MDMxNi43NjE5OTk4LCJzdWIiOiI1ZmNhMTk3YzY2YTdjMzAwM2U0Nzg0YTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.33XyUlqIMYXU2Y2nlLRiBuO5j2SlJAPOXao_dmE7mBo",
+          },
+        }
+      );
+
+      const data = await response.json();
+
+      setFilms(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // fetchMovies();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {/* {response.map()} */}
+      {/* Zorg er voor dat alle titels in een paragraaf getoond worden */}
+
+      {films.map((m) => {
+        return <p key={m.id}>{m.title}</p>;
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
